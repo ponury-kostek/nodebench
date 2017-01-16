f470a66740478f5ff83f680ee0ca040a52296cc5
Michał Żaloudik <ponury.kostek@gmail.com>
2017-01-16 11:15:51 +0100

    :sparkles:call bind apply arrow-function:sparkles:

---
+++ cba.js
@@ -0,0 +1,56 @@
+/**
+ * @author Michał Żaloudik <michal.zaloudik@redcart.pl>
+ */
+"use strict";
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite;
+suite.add('native', function () {
+	function fn(a,b) {
+		return a + b;
+	}
+	const values = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]];
+	const length = values.length;
+	for(var i = 0; i < length; i++) {
+		fn(values[i][0],values[i][1]);
+	}
+}).add('arrow function', function () {
+	function fn(a,b) {
+		return a + b;
+	}
+	const values = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]];
+	const length = values.length;
+	for(var i = 0; i < length; i++) {
+		(() => {fn(values[i][0],values[i][1])})();
+	}
+}).add('call', function () {
+	function fn(a,b) {
+		return a + b;
+	}
+	const values = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]];
+	const length = values.length;
+	for(var i = 0; i < length; i++) {
+		fn.call(null, values[i][0],values[i][1]);
+	}
+}).add("bind", function () {
+	function fn(a,b) {
+		return a + b;
+	}
+	const values = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]];
+	const length = values.length;
+	for(var i = 0; i < length; i++) {
+		fn.bind(null, values[i][0],values[i][1])();
+	}
+}).add('apply', function () {
+	function fn(a,b) {
+		return a + b;
+	}
+	const values = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]];
+	const length = values.length;
+	for(var i = 0; i < length; i++) {
+		fn.apply(null, values[i]);
+	}
+}).on('cycle', function (event) {
+	console.log(String(event.target));
+}).on('complete', function () {
+	console.log('Fastest is ' + this.filter('fastest').map('name'));
+}).run();
